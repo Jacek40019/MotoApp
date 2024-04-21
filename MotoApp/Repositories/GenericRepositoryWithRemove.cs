ddc26f88d6d7a82b19a99007a5f2791fbc7adda1
Jacek Zimoląg <jacek.zimolag1@gmail.com>
2024-04-21 19:14:20 +0200

    MotoApp - Repozytorium generyczne

---
+++ MotoApp/Repositories/GenericRepositoryWithRemove.cs
@@ -0,0 +1,13 @@
+﻿namespace MotoApp.Repositories;
+
+using MotoApp.Entities;
+
+public class GenericRepositoryWithRemove<TEntity, TKey> : GenericRepository<TEntity, TKey> where TEntity : class, IEntity, new()
+        where TKey : struct
+{
+    public void Remove(TEntity item)
+    {
+        _items.Remove(item);
+    }
+
+}
